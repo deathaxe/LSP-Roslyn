ece9238518d2cd6b93a84fc86420d23f7951f4df
deathaxe <deathaxe82@gmail.com>
2026-06-20 13:47:27 +0200

    Improve update checks
    1. check for updates once a day, only
    2. fail gracefully if updates can't be determined (offline)

--- plugin.py
+++ plugin.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import sublime
 import subprocess
+import time
 from os.path import dirname
+from shutil import which
 from typing import cast, override
 
 from LSP.plugin import LspPlugin, OnPreStartContext
@@ -21,15 +23,32 @@ class RoslynPlugin(LspPlugin):
         context.variables["package_path"] = dirname(__spec__.origin)
         context.variables["server_path"] = server_path
 
+        # check language server existence and determine last update check timestamp
+        update_timestamp = 0.0
+        update_timestamp_file = cls.plugin_storage_path / "last_update_check"
+        server_binary = which(cast(str, sublime.expand_variables(context.configuration.command[0], context.variables)))
+        if server_binary:
+            try:
+                update_timestamp = float(update_timestamp_file.read_text())
+            except Exception:
+                pass
+
         # check language server availability and install/upgrade on demand
-        subprocess.check_output(
-            args=[
-                cast(str, sublime.expand_variables(p, context.variables))
-                for p in context.configuration.install_command
-            ],
-            cwd=server_path,
-            shell=True,
-        )
+        if (now := time.time()) > update_timestamp + 24 * 60:
+            try:
+                _ = subprocess.check_output(
+                    args=[
+                        cast(str, sublime.expand_variables(p, context.variables))
+                        for p in context.configuration.install_command
+                    ],
+                    cwd=server_path,
+                    shell=True,
+                )
+            except subprocess.CalledProcessError:
+                if server_binary is None:
+                    raise
+
+            update_timestamp_file.write_text(str(now))
 
 
 def plugin_loaded() -> None:
